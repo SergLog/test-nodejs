1345d829bd12f6ed6fee3d1abb72d62df3989916
Sergey Loginov <the.loginov@gmail.com>
2019-10-09 16:01:15 +0500

    4

--- test.js
+++ test.js
@@ -4,6 +4,6 @@ http.createServer(function(request, response){
      
     response.setHeader("UserId", 12);
     response.setHeader("Content-Type", "text/html; charset=utf-8;");
-    response.write("<h2>hello world</h2>");
+    response.write("<h2>Привет, Андрей!</h2>");
     response.end();
 }).listen(process.env.PORT || 3000);

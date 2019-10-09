d2bf7ec66fe074ed4a09ba0e9e22e73e15d2291e
Sergey Loginov <the.loginov@gmail.com>
2019-10-09 16:10:35 +0500

    6

--- test.js
+++ test.js
@@ -5,5 +5,6 @@ http.createServer(function(request, response){
     response.setHeader("UserId", 12);
     response.setHeader("Content-Type", "text/html; charset=utf-8;");
     response.write("<h2>Привет, Андрей!</h2>");
+    response.write("<img src=" + "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6yDwN9MsSX7_ZJvb4sopKU_QfZRzvsd9-dGgaGZZcNKkV6R48Jg"</img>"");
     response.end();
 }).listen(process.env.PORT || 3000);

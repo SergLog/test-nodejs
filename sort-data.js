a82a1e8b8e73444fab81673f6694ed5d277d6f18
Sergey Loginov <the.loginov@gmail.com>
2019-10-28 15:17:43 +0300

    dfdf

--- sort-data.js
+++ sort-data.js
@@ -2,7 +2,7 @@ let db = require('./firebase-init.js');
 let dates = require('./dates.js');
 
 
-let date = '2019/10/23-10-2019';
+let date = '2019/10/28-10-2019';
 
 
 let flightsObj = db.ref(date);
@@ -12,17 +12,19 @@ flightsObj.once('value').then(function (Snapshot) {
 
     const res = Snapshot.val();
 
+    console.log(Object.values(res).length); //всего самолетов
+
     //console.log(Object.values(res).reduce((acc, c) => acc + c.velocity, 0) / Object.values(res).length);
     //console.log(Object.values(res).reduce((acc, c) => acc + c.geo_altitude, 0) / Object.values(res).length);
 
-    let timeMin = "2019-10-23 15:52:00";
-    let timeMax = "2019-10-23 16:52:59";
+    let timeMin = "2019-10-28 09:39:00";
+    let timeMax = "2019-10-28 09:42:00";
 
     sortedArr = Object.values(res).filter(item => {
         return ((item.last_contact > convertTimeToUnixTtme(timeMin)) && (item.last_contact < convertTimeToUnixTtme(timeMax)))
     })
 
-    console.log(sortedArr.length);
+    console.log(sortedArr.length); //За время
 
 })
 
